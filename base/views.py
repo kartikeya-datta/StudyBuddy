62228d31ac1dcb38f4b742d5f26ac0bc2a6293ca
M KARTIKEYA DATTA <kartikeyadatta3@gmail.com>
2025-04-21 15:42:00 -0500

    Created login and register

--- base/views.py
+++ base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 # rooms = [
@@ -11,6 +18,51 @@ from .forms import RoomForm
 #     {'id':3, 'name':'Frontend developers'},
 # ]
 
+def login_page(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            messages.error(request, 'User does not exist')
+            return redirect('login')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f'Welcome back, {user.username}!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Incorrect password')
+            return redirect('login')
+    context = {'page': page}
+    return render(request, 'base/login_register.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+def register_page(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            messages.success(request, 'Account created successfully!')
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration') # flash message
+            return redirect('register')
+    return render(request, 'base/login_register.html', {'form': form})
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -30,23 +82,28 @@ def room (request, pk):
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
+@login_required(login_url='login')
 def create_room(request):
     form = RoomForm()
     
     if request.method == 'POST':
         form = RoomForm(request.POST)
-        
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user  # Automatically set the host to the logged-in user
+            room.save()
             return redirect('home')
         
     context = {'form' : form}
     return render(request, 'base/room_form.html', context)
 
-
+@login_required(login_url='login')
 def update_room(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    
+    if request.user != room.host and not request.user.is_superuser:
+        return HttpResponse('You are not permitted to update this room!')
     
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
@@ -58,9 +115,12 @@ def update_room(request, pk):
     context = {'form' : form}
     return render(request, 'base/room_form.html', context)
 
-
+@login_required(login_url='login')
 def delete_room(request, pk):
     room = Room.objects.get(id=pk)
+
+    if request.user != room.host and not request.user.is_superuser:
+     return HttpResponse('You are not permitted to Delete this room!')
 
     if request.method == 'POST':
         room.delete()
