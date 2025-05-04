5bf19af11a2d7babe76a5e5a8c1c948442a77b88
M KARTIKEYA DATTA <kartikeyadatta3@gmail.com>
2025-05-04 10:32:13 -0500

    removed old database and a added a user model

--- base/models.py
+++ base/models.py
@@ -1,5 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, unique=True, null=True)
+    bio = models.TextField(null=True)
+    # avatar = models.ImageField(null=True, default="avatar.svg")
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    def __str__(self):
+        return str(self.email)
 
 # Create your models here.
 
--- base/views.py
+++ base/views.py
@@ -2,14 +2,12 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from django.http import HttpResponse
-from .models import Room, Topic, Message
+from .models import Room, Topic, Message, User
 from .forms import RoomForm, UserForm
-from django.contrib.auth.models import User
 from django.contrib import messages
 
 # Create your views here.
